561c0dc15c168a87aa1fa6767624f70312740f14
Paulo Rodrigues <pr.rodriguessrp@gmail.com>
2021-05-27 22:31:10 -0300

    apagamos transactions para goals

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,35 +1,24 @@
 class TransactionsController < ApplicationController
-    before_action :set_user
+    # before_action :set_user
 
-    def create
-        goal = Goal.find(params[:goal_id])
-        transaction  = Transaction.create!(amount: goal.penalty, state: 'pending', goal_id: goal.id, user: current_user)
+    # def create
+    #     goal = Goal.find(params[:goal_id])
+    #     transaction  = Transaction.create!(amount: goal.penalty, state: 'pending', goal_id: goal.id, user: current_user)
       
-        session = Stripe::Checkout::Session.create(
-          payment_method_types: ['card'],
-          line_items: [{
-            name: current_user.name,
-            amount: goal.penalty * 100,
-            currency: 'brl',
-            quantity: '1',
-            description: "Você só será cobrado se não cumprir sua meta dentro do prazo."
-          }],
-          success_url: transaction_url(transaction),
-          cancel_url: transaction_url(transaction)
-        )
+    #     session = Stripe::Checkout::Session.create(
+    #       payment_method_types: ['card'],
+    #       line_items: [{
+    #         name: current_user.name,
+    #         amount: goal.penalty * 100,
+    #         currency: 'brl',
+    #         quantity: '1',
+    #         description: "Você só será cobrado se não cumprir sua meta dentro do prazo."
+    #       }],
+    #       success_url: transaction_url(transaction),
+    #       cancel_url: transaction_url(transaction)
+    #     )
       
-        transaction.update(checkout_session_id: session.id)
-        redirect_to new_transaction_payment_path(transaction)
-      end
-
-      def show
-        @body_image = "body-image-transaction"
-        @transaction = Transaction.find(params[:id])
-      end
-
-      private
-
-      def set_user
-        @user = current_user
-      end
+    #     transaction.update(checkout_session_id: session.id)
+    #     redirect_to new_transaction_payment_path(transaction)
+    #   end
 end
