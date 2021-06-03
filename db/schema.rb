bf11001863ef7a2aa08bc58c360a5c3992a01bc2
Bruno Cinelli do Amaral <brunocinelli2@gmail.com>
2021-06-03 20:01:27 -0300

    criei coluna referee na tabela Users e modifiquei seeds para sermos referee

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_191309) do
+ActiveRecord::Schema.define(version: 2021_06_03_225840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_191309) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.boolean "admin", default: false, null: false
+    t.boolean "referee", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
