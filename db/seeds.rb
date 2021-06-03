4468106c76a0b73df3369d86e35910798b53281b
Bruno Cinelli do Amaral <brunocinelli2@gmail.com>
2021-06-03 20:06:44 -0300

    alterei referee no form para *

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ end
 User.create(name: 'Paulo', email: 'paulo@paulo.com', password: "123456", admin: true, referee: true)
 User.create(name: 'Marcelo', email: 'marcelo@marcelo.com', password: "123456", admin: true, referee: true)
 User.create(name: 'Bruno', email: 'bruno@gmail.com', password: "123456", admin: true, referee: true)
+User.create(name: 'Bruno', email: 'brunocinelli2@gmail.com', password: "123456", admin: true, referee: true)
 
 puts User.count
 
