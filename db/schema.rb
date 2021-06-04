f765f3119d019dd911bb504b24e6075de3f5d9b1
Bruno Cinelli do Amaral <brunocinelli2@gmail.com>
2021-06-03 21:06:59 -0300

    alterei DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_000055) do
+ActiveRecord::Schema.define(version: 2021_06_04_000535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_06_04_000055) do
     t.string "state"
     t.string "checkout_session_id"
     t.string "referee"
+    t.boolean "done"
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
