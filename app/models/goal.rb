31422ac7d6814aa613c37fe642d5eaa584b985d9
Bruno Cinelli do Amaral <brunocinelli2@gmail.com>
2021-06-05 10:50:02 -0300

    mudei foreign keys para soh belongs_to user no model de goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,7 @@
 class Goal < ApplicationRecord
-    belongs_to :owner, class_name: "User", foreign_key: "owner_id"
-    belongs_to :referee, class_name: "User", foreign_key: "referee_id"
+    belongs_to :user
+    # belongs_to :owner, class_name: "User", foreign_key: "owner_id"
+    # belongs_to :referee, class_name: "User", foreign_key: "referee_id"
     validates :deadline, :penalty, :title, :referee, presence: true
     monetize :price_cents
     # monetize :amount_cents
