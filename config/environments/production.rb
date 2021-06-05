272f9d1dae17d59376a53dc34a84fa53772f5f20
Paulo Rodrigues <pr.rodriguessrp@gmail.com>
2021-06-05 14:27:22 -0300

    host check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :test
     # host = 'example.com' #replace with your own url
-    config.action_mailer.default_url_options = { host: host }
+    config.action_mailer.default_url_options = { host: 'ulisses2021demo.herokuapp.com' }
 
     # SMTP settings for gmail
     config.action_mailer.smtp_settings = {
